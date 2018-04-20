98ffa9f8066187bc921539e0694b7a71c00e4fa1
beqa <btsalani@emis.ge>
2018-04-20 10:20:30 +0400

    added view templates

--- routes/admin.js
+++ routes/admin.js
@@ -21,35 +21,67 @@ router.get('/dashboard',adminUser.checkAuth,(req,res)=>{
 });
 
 router.get('/categories',adminUser.checkAuth,(req,res)=>{
-	res.send('hi')
+	let opt = {
+		page: 'categories',
+		user: req.user
+	};
+	res.render('admin/categories',opt);
 });
 
 router.get('/addcategorie',adminUser.checkAuth,(req,res)=>{
-	res.send('hello');
+	let opt = {
+		page: 'addcategorie',
+		user: req.user
+	};
+	res.render('admin/addcategorie',opt);
 });
 
 router.get('/movies',adminUser.checkAuth,(req,res)=>{
-	res.send('hi')
+	let opt = {
+		page: 'movies',
+		user: req.user
+	};
+	res.render('admin/movies',opt);
 });
 
 router.get('/addmovie',adminUser.checkAuth,(req,res)=>{
-	res.send('hello');
+	let opt = {
+		page: 'addmovie',
+		user: req.user
+	};
+	res.render('admin/addmovie',opt);
 });
 
 router.get('/directors',adminUser.checkAuth,(req,res)=>{
-	res.send('hi')
+	let opt = {
+		page: 'directors',
+		user: req.user
+	};
+	res.render('admin/directors',opt);
 });
 
 router.get('/adddirector',adminUser.checkAuth,(req,res)=>{
-	res.send('hello');
+	let opt = {
+		page: 'adddirector',
+		user: req.user
+	};
+	res.render('admin/adddirector',opt);
 });
 
 router.get('/actors',adminUser.checkAuth,(req,res)=>{
-	res.send('hi')
+	let opt = {
+		page: 'actors',
+		user: req.user
+	};
+	res.render('admin/actors',opt);
 });
 
 router.get('/addactor',adminUser.checkAuth,(req,res)=>{
-	res.send('hello');
+	let opt = {
+		page: 'addactor',
+		user: req.user
+	};
+	res.render('admin/addactor',opt);
 });
 
 
